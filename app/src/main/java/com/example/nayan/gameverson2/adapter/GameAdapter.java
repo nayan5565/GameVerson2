4e1bf77d4c8eb13c6facb16c898a87e489ab9dee
Nayan <nayan5565@gmail.com>
2017-02-16 02:04:43 +0600

    image changed

--- app/src/main/java/com/example/nayan/gameverson2/adapter/GameAdapter.java
+++ app/src/main/java/com/example/nayan/gameverson2/adapter/GameAdapter.java
@@ -98,8 +98,8 @@ public class GameAdapter extends RecyclerView.Adapter<GameAdapter.MyViewholder> 
             holder.txtContents.setText(mContents.getTxt());
             holder.txtContents.setTextColor(0xffff00ff);
             if (mContents.getMatch() == 1) {
-                holder.txtContents.setBackgroundColor(Color.BLACK);
-//                holder.imgAnim.setVisibility(View.VISIBLE);
+//                holder.txtContents.setBackgroundColor(Color.BLACK);
+                holder.imgAnim2.setImageResource(R.drawable.red_panel);
             } else {
                 holder.txtContents.setBackgroundColor(Color.TRANSPARENT);
             }
